21e2e4b6760dbc9dd41388a69a4de99e21a14f87
lianyongliang <lianyongliang@mudu.tv>
2020-08-11 10:51:51 +0800

    fix install

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@ install() {
     name=$1
     path=$2
     echo "Installing ${name}..."
-    export GO111MODULE=on
-    go install ${path}
-    echo ""
+    GO111MODULE=on go get ${path}
+    echo "${name} installed"
 }
 
 install timestamp github.com/gg-tools/timestamp
